92d687879ca71eaf5efffcb833e3bcb4f22ae145
darth_coder <33444220+saket0565@users.noreply.github.com>
2020-12-19 23:00:21 +0530

    Create Container With Most Water_medium.cpp

---
+++ Container With Most Water_medium.cpp
@@ -0,0 +1,15 @@
+#define ll long long int
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        ll i=0, j=height.size()-1, ans=0;
+        while(i<j){
+            ans=max(ans, min(height[i], height[j])*(j-i));
+            if(height[i]<=height[j])
+                i++;
+            else
+                j--;
+        }
+        return ans;
+    }
+};
