61ce733b521efdaafbcbe7cc384c28132de741ea
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-04-28 14:31:51 +0530

    Create Regular Expression Matching_hard.py

---
+++ Regular Expression Matching_hard.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        dp = [[False] * n] * m
+
+        dp[0][0] = True
+        for j in range(1,n + 1):
+            if p[j-1] == '*' or (j < n and p[j] == '*'):
+                dp[0][j] = True
+            else:
+                break
+        for i in range(1,m + 1):
+            for j in range(1,n + 1):
+                if p[ j - 1] == '*':
+                    dp[i][j] = (dp[i - 1][j] or dp[i][j - 2]) if (s[i - 1] == p[j - 2] or p[j - 2] == '.') else dp[i][j - 2]
+                else
+                    dp[i][j] = dp[i - 1][j - 1] if (s[i - 1] == p[j - 1] or p[j - 1] == '.') else False
+
+        return dp[m][n]
