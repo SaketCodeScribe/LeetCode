87466bcbc783e8d95fcc26a57facc16c9fc2fcc0
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-04-29 13:47:59 +0530

    Create Valid Parentheses_easy.py

---
+++ Valid Parentheses_easy.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        de = deque()
+
+        for ch in s:
+            if ch != '}' and ch != ']' and ch != ')':
+                de.append(ch)
+            else:
+                if len(de) == 0 or (ch == '}' and de[len(de) - 1] != '{') or (ch == ']' and de[len(de) - 1] != '[') or (
+                        ch == ')' and de[len(de) - 1] != '('):
+                    return False;
+                de.pop()
+
+        return True if len(de) == 0 else False
