5f6296f61cf227e2b7335d3160b354bae6fad9b2
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-05-01 12:42:30 +0530

    Update Longest Valid Parentheses_hard.py

--- Longest Valid Parentheses_hard.py
+++ Longest Valid Parentheses_hard.py
@@ -1,3 +1,4 @@
+# 1. Using Stack
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         ans = 0
@@ -16,5 +17,20 @@ class Solution:
                     ans = max(ans, i - de[len(de) - 1]) if len(de) != 0 else max(ans, i + 1)
                 else:
                     de.append(i)
+
+        return ans
+# 2.  Using dp
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * n
+        ans = 0
+
+        for i in range(1, n):
+            if s[i] == ')' and s[i - 1] == '(':
+                dp[i] = dp[i - 2] + 2;
+            elif s[i] == ')' and s[i - 1] == ')' and i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(':
+                dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
+            ans = max(ans, dp[i])
 
         return ans
