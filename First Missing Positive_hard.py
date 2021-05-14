976bb0df7d837b8027dbb0b7107960c8a0c88712
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-05-15 01:18:00 +0530

    Update First Missing Positive_hard.py

--- First Missing Positive_hard.py
+++ First Missing Positive_hard.py
@@ -1,3 +1,4 @@
+# 1. Stack based solution
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
@@ -13,3 +14,25 @@ class Solution:
             if nums[i] > 0:
                 return i + 1
         return n + 1
+
+#2. DP based solution
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        ans = 0
+        if n == 0:
+            return ans
+        l_max, r_max = [0] * n, [0] * n
+
+        mx = height[0]
+        for i in range(1, n):
+            l_max[i] = mx
+            mx = max(mx, height[i])
+        mx = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            r_max[i] = mx
+            mx = max(mx, height[i])
+        for i in range(n):
+            area = min(l_max[i], r_max[i]) - height[i]
+            ans = ans + (area if area > 0 else 0)
+        return ans
