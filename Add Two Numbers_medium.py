bf948bb595a55ddd37a33ed624af9cc6f6f217e9
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-05-18 16:44:46 +0530

    Update Add Two Numbers_medium.py

--- Add Two Numbers_medium.py
+++ Add Two Numbers_medium.py
@@ -1,8 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+'''
+    Time: O(max(m,n))
+    Space: O(1)
+'''
+
+# Definition
+# for singly - linked list.
+#     class ListNode:
+#         def __init__(self, val=0, next=None):
+#             self.val = val
+#             self.next = next
 class Solution:
     def Length(self, l1: ListNode) -> int:
         length = 0
