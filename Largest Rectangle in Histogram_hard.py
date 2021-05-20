c306bc073b4d0aa64883b8a79899e9541a6e22f8
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-05-20 15:38:22 +0530

    Update Largest Rectangle in Histogram_hard.py

--- Largest Rectangle in Histogram_hard.py
+++ Largest Rectangle in Histogram_hard.py
@@ -1,3 +1,8 @@
+'''
+    1. Using Stack
+       Time: O(n)
+       Space: O(n)
+'''
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n, ans, stack = len(heights), 0, []
@@ -17,4 +22,28 @@ class Solution:
             ht = heights[stack.pop()]
             m = m - 1
             ans = max(ans, ht * (n - stack[m - 1] - 1) if m > 0 else ht * n)
+        return ans
+'''
+   2.  Using dp
+       Time: O(n)
+       Space: O(n)
+'''
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n, ans = len(heights), 0
+        l_max, r_max = [-1] * n, [n] * n
+
+        for i in range(1, n):
+            p = i - 1
+            while p >= 0 and heights[p] >= heights[i]:
+                p = l_max[p]
+            l_max[i] = p
+        for i in range(n - 2, -1, -1):
+            p = i + 1
+            while p < n and heights[p] >= heights[i]:
+                p = r_max[p]
+            r_max[i] = p
+        for i in range(n):
+            ans = max(ans, heights[i] * (r_max[i] - l_max[i] - 1))
+
         return ans
