b25f4e6ab204fc2cb7f7eaf0d01b6199e2982bd2
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-06-03 17:36:52 +0530

    Update Word Ladder_hard.py

--- Word Ladder_hard.py
+++ Word Ladder_hard.py
@@ -1,6 +1,6 @@
 '''
-    Time Complexity: O(beginWord.length*26*n)
-    Space Complexity: O(n)
+    1. Time Complexity: O(beginWord.length*26*n)
+       Space Complexity: O(n)
 '''
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
@@ -27,6 +27,42 @@ class Solution:
                             queue.append(aux)
                             del mp[aux]
                     word[i] = ch
+                l_q -= 1
+            l_q = len(queue)
+        return 0
+
+'''
+    2. Time Complexity: O(beginWord.length*n^2)
+       Space Complexity: O(n)
+'''
+class Solution:
+    def diff(self, str1: str, str2: str) -> bool:
+        cnt = 0
+
+        for i, j in zip(str1, str2):
+            if i != j:
+                cnt += 1
+        return True if cnt == 1 else False
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord in wordList == False:
+            return 0
+        n, ans, l = len(wordList), 999999, 1
+        vis, queue = [False for i in range(n)], deque()
+
+        queue.append(beginWord)
+        l_q = len(queue)
+        while l_q > 0:
+            l += 1
+            while l_q > 0:
+                ind = queue.popleft()
+                word = wordList[ind] if ind != beginWord else ind
+                for i in range(n):
+                    if wordList[i] == endWord and self.diff(wordList[i], word):
+                        return l
+                    elif vis[i] == False and self.diff(wordList[i], word):
+                        queue.append(i)
+                        vis[i] = True
                 l_q -= 1
             l_q = len(queue)
         return 0
