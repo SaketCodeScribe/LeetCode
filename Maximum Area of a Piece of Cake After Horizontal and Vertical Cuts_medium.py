f4b8923c44bd9af508509dd39557b0cafd8d0c03
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-06-03 19:13:41 +0530

    Create Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts_medium.py

---
+++ Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts_medium.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        m, n, l, b = len(horizontalCuts), len(verticalCuts), 0, 0
+
+        horizontalCuts.sort()
+        verticalCuts.sort()
+        l, b = max(horizontalCuts[0], h - horizontalCuts[m - 1]), max(verticalCuts[0], w - verticalCuts[n - 1])
+        for i in range(1, m):
+            l = max(l, horizontalCuts[i] - horizontalCuts[i - 1])
+        for i in range(1, n):
+            b = max(b, verticalCuts[i] - verticalCuts[i - 1])
+        return (l * b) % (10 ** 9 + 7)
