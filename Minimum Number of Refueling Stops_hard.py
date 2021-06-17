a47cfb11cbfc2afdd2d8de23296d2ecc7b3e5f0e
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-06-17 15:40:41 +0530

    Update Minimum Number of Refueling Stops_hard.py

--- Minimum Number of Refueling Stops_hard.py
+++ Minimum Number of Refueling Stops_hard.py
@@ -1,5 +1,26 @@
 '''
-    1. Time Complexity: O(n^2)
+    1. Time Complexity: O(nlogn)
+       Space Complexity: O(n)
+'''
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        stations.append([target,0])
+        ans, prev, tank = 0, 0, startFuel
+        heap = []
+
+        for loc,fuel in stations:
+            tank -= loc - prev
+            while len(heap) != 0 and tank < 0:
+                tank += -heapq.heappop(heap)
+                ans += 1
+            if tank < 0:
+                return -1
+            heapq.heappush(heap,-fuel)
+            prev = loc
+        return ans
+
+'''
+    2. Time Complexity: O(n^2)
        Space Complexity: O(n)
 '''
 class Solution:
