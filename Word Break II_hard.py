82e34212895fdc2eaf8ac812bde5310e8e61885b
darth_coder <33444220+saket0565@users.noreply.github.com>
2021-07-04 22:51:15 +0530

    Update Word Break II_hard.py

--- Word Break II_hard.py
+++ Word Break II_hard.py
@@ -1,23 +1,29 @@
+'''
+    Time Complexity: O(n*2^n)
+    Space Complexity: O(n*2^n)
+'''
 class Solution:
     def __init__(self, ) -> None:
         self.ans = None
         self.dic = None
 
     def WordBreak(self, s, temp, i, n):
-        s1 = []
+        m, s1 = 0, []
         if i >= n:
             self.ans.append(''.join(temp[:-1]))
             return None
         while i < n:
             s1.append(s[i])
-            if self.dic[''.join(s1)] > 0:
-                m = len(s1)
+            m += 1
+            if m <= 10 and self.dic[''.join(s1)] > 0:
                 for j in range(m):
                     temp.append(s1[j])
                 temp.append(' ')
                 self.WordBreak(s, temp, i + 1, n)
                 for j in range(m + 1):
                     temp.pop()
+            elif m > 10:
+                return None
             i += 1
         return None
 
