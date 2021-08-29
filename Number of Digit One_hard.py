6d4922ac942dea66d98fba4c467740052464f944
mobius <saket0565@gmail.com>
2021-08-29 17:22:18 +0530

    Create Number of Digit One_hard.py

---
+++ Number of Digit One_hard.py
@@ -0,0 +1,12 @@
+'''
+    Time Complexity: O(log n)
+    Space Complexity: O(n)
+'''
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        ans, div, mul = 0, 10, 1
+        while n//mul > 0:
+            ans += (n//div*mul) + min(max(n%div - mul + 1, 0), mul)
+            div *= 10
+            mul *= 10
+        return ans
