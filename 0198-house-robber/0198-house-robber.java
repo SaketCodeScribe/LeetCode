c394841457621d7d392e349ca2db1ef30d748aae
Saket Suman <33444220+SaketCodeScribe@users.noreply.github.com>
2023-10-30 22:20:16 +0530

    Time: 0 ms (100%), Space: 39.6 MB (68.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] arr) {
+        int i, n = arr.length, prev = -1, curr, pprev = 0;
+        prev = arr[0];
+        
+        for(i=1; i<n; i++){
+            curr = Math.max(pprev+arr[i], prev);
+            pprev = Math.max(prev, pprev);
+            prev = curr;
+        }
+        return prev;
+    }
+}
